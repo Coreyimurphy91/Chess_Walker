cc84ae869defe744d776b8b753eaaef00fde0a78
Corey Murphy <coreyimurphy@gmail.com>
2022-07-30 13:44:54 -0400

    toying with start menu

--- app.js
+++ app.js
@@ -4,7 +4,11 @@ const map = document.querySelector('.map');
 const movement = document.getElementById('movement');
 const canvas = document.getElementById('canvas');
 const status = document.getElementById('status');
+const startMenuImg = document.getElementById('help');
 const ctx = canvas.getContext('2d'); //creates a 2D canvas
+const img = document.createElement('img');
+const startMenu = new Image ();
+startMenu.src = 'images/guide.png';
 let knight;
 let rook;
 let bishop;
@@ -16,19 +20,37 @@ const kPiece = document.getElementById('kPiece');
 const bgBlack = document.getElementById('bgBlack');
 const bgWhite = document.getElementById('bgWhite');
 
-// EVENT LISTENERS
+// ---------------- EVENT LISTENERS ----------  //
 window.addEventListener('DOMContentLoaded', function() {
 
-    const runGame = setInterval(gameLoop, 60);
+    // const startGame = setInterval(gameMenu, 60);
+    const rungame = setInterval(gameLoop, 60);
 });
 
 
 // ---------------- Canvas Rendering ----------  //
 canvas.width = 840;
 canvas.height = 840;
-//    map.style.transform = `translate3d(1368px,1363px, 0 )`;
-// canvas.setAttribute('height', getComputedStyle(canvas)['height']);
-// canvas.setAttribute('width', getComputedStyle(canvas)['width']);
+
+// ---------------- Buttons -------------------  //
+document.getElementById('btm-right').onmousedown = function() {
+    document.getElementById('btm-right').style.color = 'rgb(44, 77, 114)';
+    document.getElementById('btm-right').style.backgroundColor = 'rgba(210, 177, 140, .15)';
+    guide.style.display = "inline";
+    document.querySelector('body').appendChild(img);
+}
+document.getElementById('btm-right').onmouseleave = function() {
+    document.getElementById('btm-right').style.color = 'rgb(210, 177, 140)';
+    document.getElementById('btm-right').style.backgroundColor = 'rgba(210, 177, 140, .25)';
+    guide.style.display = "none";
+    document.querySelector('body').removeChild(img);
+}
+function guideFunction() {
+    const popup = document.getElementById("myPopup");
+    popup.classList.toggle("show");
+  }
+
+// ---------------- Event Listeners -----------  //
 
 // ---------------- Entities ------------------  //
 
@@ -48,9 +70,10 @@ class Player {
     }
 }
 
-knight = new Player(365, 365, kPiece, 100, 100);
+knight = new Player(365, 370, kPiece, 100, 100);
 bgBlacksquare = new Player(0, 0, bgBlack, 840, 840)
 bgWhitesquare = new Player(0, 0, bgWhite, 840, 840)
+imggg = new Player(0,0, img, 840, 840)
 
 
 
@@ -63,18 +86,18 @@ function movementHandler(e) { // e just means event (what keydown recognizes)
     console.log('movement', e.key)
 
     switch(e.key) {
-        case 'r': 
-            keys.r.pressed = true;
-            if (knight.x < 590 && knight.y > 30) {
+        case 'e': 
+            // keys.e.pressed = true;
+            if (knight.x < 590 && knight.y > 35) {
                 knight.x += 228;
                 knight.y -= 114;
             } // solved
             // knight.x < 590 ? (knight.x += 228) : null;
             // knight.y > 252 ? (knight.y -= 114) : null;
                 break;
-        case 'f': 
-            keys.r.pressed = true;
-            if (knight.x < 590 && knight.y < 700) {
+        case 'd': 
+            // keys.d.pressed = true;
+            if (knight.x < 590 && knight.y < 705) {
                 knight.x += 228;
                 knight.y += 114;
             } // solved
@@ -82,8 +105,8 @@ function movementHandler(e) { // e just means event (what keydown recognizes)
             // knight.y < 478 ? (knight.y += 114) : null;
                 break;
         case 'c': 
-            keys.r.pressed = true;
-            if (knight.x < 700 && knight.y < 590) {
+            // keys.c.pressed = true;
+            if (knight.x < 700 && knight.y < 595) {
                 knight.x += 114;
                 knight.y += 228;
             } // solved
@@ -91,26 +114,26 @@ function movementHandler(e) { // e just means event (what keydown recognizes)
             // knight.y += 228;
                 break;
         case 'x': 
-            keys.r.pressed = true;
-            if (knight.x > 130 && knight.y < 590) {
+            // keys.x.pressed = true;
+            if (knight.x > 130 && knight.y < 595) {
                 knight.x -= 114;
                 knight.y += 228;
             } // solved
             // knight.x -= 114;
             // knight.y += 228;
                 break;
-        case 'a': 
-            keys.r.pressed = true;
-            if (knight.x > 250 && knight.y < 700 ) {
+        case 's': 
+            // keys.s.pressed = true;
+            if (knight.x > 250 && knight.y < 705 ) {
                 knight.x -= 228;
                 knight.y += 114;
             } // solved
             // knight.x -= 228;
             // knight.y += 114;
                 break;
-        case 'q': 
-            keys.r.pressed = true;
-            if (knight.x > 250 && knight.y > 30) {
+        case 'w': 
+            // keys.w.pressed = true;
+            if (knight.x > 250 && knight.y > 35) {
                 knight.x -= 228;
                 knight.y -= 114;
             } // solved
@@ -118,8 +141,8 @@ function movementHandler(e) { // e just means event (what keydown recognizes)
             // knight.y -= 114;
                 break;
         case '2': 
-            keys.r.pressed = true;
-            if (knight.x > 30 && knight.y > 140) {
+            // keys.2.pressed = true;
+            if (knight.x > 30 && knight.y > 145) {
                 knight.x -= 114;
                 knight.y -= 228;
             } // solved
@@ -127,8 +150,8 @@ function movementHandler(e) { // e just means event (what keydown recognizes)
             // knight.y -= 228;
                 break;
         case '3': 
-            keys.r.pressed = true;
-            if (knight.x < 700 && knight.y > 140) {
+            // keys.3.pressed = true;
+            if (knight.x < 700 && knight.y > 145) {
                 knight.x += 114;
                 knight.y -= 228;
             } // solved
@@ -146,16 +169,16 @@ const keys = {
     3: {
         pressed: false
     },
-    q: {
+    w: {
         pressed: false
     },
-    r: {
+    e: {
         pressed: false
     },
-    a: {
+    s: {
         pressed: false
     },
-    f: {
+    d: {
         pressed: false
     },
     x: {
@@ -166,14 +189,20 @@ const keys = {
     }
 };
 
+function startGame() {
+    window.requestAnimationFrame(gameMenu)
+    imggg.render();
+}
+
 function gameLoop() {
     // Call window.requestAnimationFrame() and pass in animate to refresh the canvas constantly
     window.requestAnimationFrame(gameLoop)
-    // check to see if shrek is alive
+    // check to see if knight is alive
     if (knight.alive) {
         // render shre
         // bgWhitesquare.render();
         bgBlacksquare.render();
+        // imggg.render();
         knight.render();
         // @todo - check for collision
         // let hit = detectHit(donkey, shrek);
